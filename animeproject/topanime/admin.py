499a013160752a1589b869a6fb3751532321662e
HeisenDelta <ariya8.narayan7@outlook.jp>
2023-06-29 19:25:33 +0900

    created Anime model and updated with default 4 anime

--- animeproject/topanime/admin.py
+++ animeproject/topanime/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Anime
 
-# Register your models here.
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = ('TITLE', 'DESCR', 'IMGPATH', 'SHOWN')
+
+admin.site.register(Anime, AnimeAdmin)
--- animeproject/topanime/models.py
+++ animeproject/topanime/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Anime(models.Model):
+    TITLE   = models.CharField(max_length = 100)
+    DESCR   = models.CharField(max_length = 300)
+    IMGPATH = models.CharField(max_length = 300)
+    SHOWN   = models.BooleanField(default = False)
+
+    def __str__(self) -> str:
+        imgpath = self.IMGPATH[:12] + '...' if len(self.IMGPATH) > 15 else self.IMGPATH
+        return f'{self.TITLE} at {imgpath}'
